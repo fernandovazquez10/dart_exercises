16accc67f37a1e1c5f6c203654b4e307f965e07e
Alonso <f.vazquez151000@gmail.com>
2023-08-09 00:09:54 -0600

    :white_check_mark: 01-intro

---
+++ 01_hello_word.dart
@@ -0,0 +1,8 @@
+void main(){
+  // var myName = "Alonso"; it is better to define the type of variable although dart can also infer it
+  String myName = "Alonso";
+  // myName = 2; Error because dart has strong typing
+
+  print("Hello world! my name is $myName");
+  print("Hello world! my name in upper case is ${myName.toUpperCase()}}");
+}
