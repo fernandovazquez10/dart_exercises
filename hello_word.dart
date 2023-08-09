407cd79b8b472e5da521bdf79242dc05961aa74b
Alonso <f.vazquez151000@gmail.com>
2023-08-08 23:46:30 -0600

    Hello word

---
+++ hello_word.dart
@@ -0,0 +1,7 @@
+void main(){
+  // var myName = "Alonso"; it is better to define the type of variable although dart can also infer it
+  String myName = "Alonso";
+  // myName = 2; Error because dart has strong typing
+
+  print("Hello world! my name is $myName");
+}
